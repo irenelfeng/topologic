42d08a1c73b127aa9d598bc1b334622924540da2
Jessica Fan <Jessica@jessicas-air-5.wireless.dartmouth.edu>
2016-02-19 17:00:41 -0500

    style files

--- app/components/content/form-view/form-components/title-component.js
+++ app/components/content/form-view/form-components/title-component.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export default class TitleComponent extends React.Component { 
+export default class TitleComponent extends React.Component {
   constructor() {
     super();
   }
