e3cce4ebbfba4df7ccd8acf272ca854f6ff2d41e
ben-pr-p <ben.paul.ryan.packer@gmail.com>
2016-02-20 16:33:52 -0500

    new style project view

--- app/components/content/form-view/form-components/task-or-project.js
+++ app/components/content/form-view/form-components/task-or-project.js
@@ -3,29 +3,35 @@ import React from 'react';
 export default class TaskOrProject extends React.Component {
   constructor() {
     super();
-
-    // this.state = {
-    //   selected: 'task'
-    // };
   }
 
   onClick(e) {
     var option = e.target.id.split('-')[0];
-    // this.setState({ selected: option });
-    //change the form
-    var type = 'new'+option
+    var type = 'new' + option;
     this.props.changeForm(type);
-    
   }
 
   render() {
-
     var s = (type) => this.props.type == type ? 'selected' : '';
 
     return (
-      <div id="task-or-proj-container">
-        <div id="task-option" className={'option ' + s('task')} onClick={this.onClick.bind(this)}> Task </div>
-        <div id="project-option" className={'option ' + s('project')} onClick={this.onClick.bind(this)}> Project </div>
+      <div id="task-or-project-container">
+
+        <div id="task-or-project-header">
+          <div className="text-container"> New... </div>
+        </div>
+
+        <div id="option-containers">
+
+          <div id="task-option" className={'option ' + s('task')} onClick={this.onClick.bind(this)}>
+            <div className="text-container"> Task </div>
+          </div>
+
+          <div id="project-option" className={'option ' + s('project')} onClick={this.onClick.bind(this)}>
+            <div className="text-container"> Project </div>
+          </div>
+
+        </div>
       </div>
     );
   }
