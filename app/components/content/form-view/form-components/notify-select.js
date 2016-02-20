2461f1748e5284bbdfe8032fc8082d25754547ff
Jessica Fan <Jessica@jessicas-air-5.wireless.dartmouth.edu>
2016-02-20 16:30:46 -0500

    new task form col2 larger

--- app/components/content/form-view/form-components/notify-select.js
+++ app/components/content/form-view/form-components/notify-select.js
@@ -10,7 +10,11 @@ export default class NotifySelect extends React.Component {
       var deadlineEls =
       (<div className="form-aligned-col2-list-item">
         <input type="checkbox" className="form-aligned-col2-check" />
-        <div className="form-aligned-col2-text"> 15 mins before the deadline </div>
+        <div className="form-aligned-col2-notif">
+          <input className="notif-num" placeholder="15" />
+          <div className="notif-dropdown"> minutes </div>
+          <div className="notif-text"> before the deadline </div>
+        </div>
       </div>)
     }
     else deadlineEls ='';
@@ -19,7 +23,7 @@ export default class NotifySelect extends React.Component {
       <div>
         <div id="notify-select" className="form-group">
           <div className="form-aligned-col1"> Notify me... </div>
-          <div className="form-aligned-col2-list">
+          <div className="form-algined-col2-list">
             <div className="form-aligned-col2-list-item">
               <input type="checkbox" className="form-aligned-col2-check" />
               <div className="form-aligned-col2-text"> When task is completed </div>
