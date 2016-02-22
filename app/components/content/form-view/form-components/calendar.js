77597e7c5d2c4885bb79880bd62aa6d81cc7774c
Jessica Fan <Jessica@jessicas-air-5.wireless.dartmouth.edu>
2016-02-22 02:24:16 -0500

    imported modal and timepicker module

--- app/components/content/form-view/form-components/calendar.js
+++ app/components/content/form-view/form-components/calendar.js
@@ -1,26 +1,68 @@
 import React from 'react';
 import DatePicker from 'react-date-picker';
+import TimePicker from 'rc-time-picker';
+import Modal from 'react-modal';
 
 export default class Calendar extends React.Component {
   constructor() {
     super();
     this.state = {
       picker: false,
+      timePicker: false,
       date: ''
     }
+    this.customStyles = {
+      content : {
+        position                   : 'absolute',
+        top                        : '20%',
+        left                       : '40%',
+        right                      : '20%',
+        bottom                     : '20%',
+        background                 : '#29C8E1',
+        borderRadius               : '10px',
+        outline                    : 'none'
+      }
+    };
   }
 
   onClick(e) {
-    this.setState({picker: !this.state.picker});
+    this.setState({picker: true});
   }
 
   onChange(date) {
     this.setState({date});
   }
 
+  onCheck(e) {
+    this.setState({timePicker: e.target.checked});
+  }
+
+  doneClick() {
+    this.setState({picker: false});
+  }
+
+
   render() {
-    if (this.state.picker)
-      var pickerEls = (<DatePicker hideFooter={true} navPrev={"<<"} navNext={">>"} onChange={this.onChange.bind(this)}/>);
+    if (this.state.picker) {
+      var date = (<DatePicker hideFooter={true} navPrev={"<<"} navNext={">>"} onChange={this.onChange.bind(this)}/>);
+      var addTime =
+        (<div className="form-group">
+          <input type="checkbox" className="form-aligned-col2-check" onClick={this.onCheck.bind(this)}/>
+          <div className="form-aligned-col2-text">Add time</div>
+        </div>);
+
+      if (this.state.timePicker)
+        var time = (<TimePicker showSecond={false} />)
+      else time = '';
+
+      var done =
+        (<div className="form-group">
+          <a className="form-button" onClick={this.doneClick.bind(this)}>
+            Done
+          </a>
+        </div>);
+      var pickerEls = (<Modal isOpen={this.state.picker} onRequestClose={this.closeModal} style={this.customStyles}> {date} {addTime} {time} {done} </Modal>);
+    }
     else pickerEls = '';
 
     return (
