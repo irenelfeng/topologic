360649496e41f018eb8bab7deba1d0be5532901b
irenelfeng <irene.l.feng.17@dartmouth.edu>
2016-02-22 02:17:24 -0500

    selectize and group select

--- app/components/content/form-view/form-components/group-select.js
+++ app/components/content/form-view/form-components/group-select.js
@@ -1,16 +1,27 @@
 import React from 'react';
 
-export default class Title extends React.Component { 
+var rs = require('react-selectize');
+import ReactSelectize from 'react-selectize';
+
+export default class GroupSelect extends React.Component { 
   constructor() {
     super();
   }
 
   render() {
+    var SimpleSelect = ReactSelectize.SimpleSelect;
+    var options = ["testing", "this", "whee"].map(function(val){
+                return {label: val, value: val}
+            });
+
     return (
       <div id="group-select" className="form-group">
         <div className="form-aligned-col1"> Group: </div>
         <div id="group-dropdown" className="form-aligned-col2">
-          Sample Group
+          <SimpleSelect 
+            options = {options} 
+            placeholder = "Select a group"
+            />
         </div>
       </div>
     );
--- app/components/content/form-view/form-components/members.js
+++ app/components/content/form-view/form-components/members.js
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import ReactSelectize from 'react-selectize';
 export default class Members extends React.Component {
   constructor() {
     super();
@@ -10,15 +11,18 @@ export default class Members extends React.Component {
     this.props.addMember(test);
   }
 
+  removeMember(i, e){
+    this.props.removeMember(i);
+  }
+
   render() {
     var members;
     if(this.props.members)
-      members = this.props.members.map((m, i) => (<div>{m}<span className="remove-member" onClick={this.props.removeMember(i)}>x</span></div>));
+      members = this.props.members.map((m, i) => (<div>{m}<span className="remove-member" onClick={this.removeMember.bind(this, i)}>x</span></div>));
 
     return (
       <div>
         <div className="add-box" onClick={this.addMember.bind(this)} >
-          Add Member
         </div>
         <div id="members-box">
           {members}
